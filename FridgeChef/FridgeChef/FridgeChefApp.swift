734975dd7fface231daeaedca5686887c53c408c
Vickyoyayalo <168822218+Vickyoyayalo@users.noreply.github.com>
2024-09-13 17:25:57 +0800

    Import Firebase SDK and GoogleService.plist

---
+++ FridgeChef/FridgeChef/FridgeChefApp.swift
@@ -0,0 +1,33 @@
+//
+//  FridgeChefApp.swift
+//  FridgeChef
+//
+//  Created by Vickyhereiam on 2024/9/13.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
+
+
+@main
+struct FridgeChefApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                FridgePageView()
+            }
+        }
+    }
+}

37df0a822f575cbaaec0c639922be5e7f6bcf2aa
Vickyoyayalo <168822218+Vickyoyayalo@users.noreply.github.com>
2024-09-21 10:53:17 +0800

    Fix MapViewUserLocation Close ButtonUI

--- FridgeChef/FridgeChef/View/MapViewWithUserLocation.swift
+++ FridgeChef/FridgeChef/View/MapViewWithUserLocation.swift
@@ -14,44 +14,50 @@ struct MapViewWithUserLocation: View {
     @State private var trackingMode: MapUserTrackingMode = .follow // 使用追踪模式的绑定属性
     
     var body: some View {
-        if let userLocation = locationManager.lastKnownLocation {
-            // 显示用户的当前位置并跟踪用户
-            Map(coordinateRegion: .constant(MKCoordinateRegion(
-                center: userLocation.coordinate,
-                span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
-            )), showsUserLocation: true, userTrackingMode: $trackingMode) // 使用 $trackingMode 绑定来跟踪用户
-            .edgesIgnoringSafeArea(.all)
-        } else {
-            // 提示用户授予定位权限
-            Text("定位中...")
-                .onAppear {
-                    locationManager.requestPermission() // 请求定位权限
+        ZStack {
+            if let userLocation = locationManager.lastKnownLocation {
+                // 显示用户的当前位置并跟踪用户
+                Map(coordinateRegion: .constant(MKCoordinateRegion(
+                    center: userLocation.coordinate,
+                    span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+                )), showsUserLocation: true, userTrackingMode: $trackingMode) // 使用 $trackingMode 绑定来跟踪用户
+                .edgesIgnoringSafeArea(.all)
+            } else {
+                // 提示用户授予定位权限
+                Text("定位中...")
+                    .onAppear {
+                        locationManager.requestPermission() // 请求定位权限
+                    }
+                    .alert(isPresented: $locationManager.showAlert) {
+                        Alert(
+                            title: Text("需要定位權限"),
+                            message: Text("請在設置中允許訪問位置信息"),
+                            primaryButton: .default(Text("打開設置"), action: {
+                                if let url = URL(string: UIApplication.openSettingsURLString) {
+                                    UIApplication.shared.open(url)
+                                }
+                            }),
+                            secondaryButton: .cancel()
+                        )
+                    }
+            }
+            VStack{
+                Spacer()
+                Button(action: {
+                    isPresented = false // 设置为 false 来关闭 sheet
+                }) {
+                    Image(systemName: "xmark.circle.fill")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                        .padding()
+                        .background(Color.white.opacity(0.8))
+                        .shadow(radius: 5)
+                        .clipShape(Circle())
                 }
-                .alert(isPresented: $locationManager.showAlert) {
-                    Alert(
-                        title: Text("需要定位權限"),
-                        message: Text("請在設置中允許訪問位置信息"),
-                        primaryButton: .default(Text("打開設置"), action: {
-                            if let url = URL(string: UIApplication.openSettingsURLString) {
-                                UIApplication.shared.open(url)
-                            }
-                        }),
-                        secondaryButton: .cancel()
-                    )
-                }
-        }
-        // 添加一个关闭按钮
-        Button(action: {
-            isPresented = false // 设置为 false 来关闭 sheet
-        }) {
-            Image(systemName: "xmark.circle.fill")
-                .resizable()
-                .frame(width: 40, height: 40)
                 .padding()
-                .background(Color.white.opacity(0.7))
-                .clipShape(Circle())
-                .shadow(radius: 5)
+                .background(Color.clear)
+            }
+            .edgesIgnoringSafeArea(.all)
         }
-        .padding()
     }
 }

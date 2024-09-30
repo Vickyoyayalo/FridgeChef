c63af43b8f87fb0cdc41399f20514ad4ce25231b
Vickyoyayalo <168822218+Vickyoyayalo@users.noreply.github.com>
2024-09-30 18:18:54 +0800

    Login all done and fixed the push view bug!

--- FridgeChef/FridgeChef/View/LoginDetailView.swift
+++ FridgeChef/FridgeChef/View/LoginDetailView.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Vickyhereiam on 2024/9/16.
 //
+
 import SwiftUI
 import AuthenticationServices
 import Firebase
@@ -12,7 +13,7 @@ import FirebaseAuth
 
 struct LoginDetailView: View {
     @StateObject private var loginViewModel = LoginDetailViewModel()
-    @StateObject private var userViewModel = UserViewModel() 
+    @StateObject private var userViewModel = UserViewModel()
     @State private var navigateToHome = false
     @State private var navigateToForgotPassword = false
     @State private var isLoggedIn = false
@@ -46,11 +47,11 @@ struct LoginDetailView: View {
                         .padding()
                         .background(Color(UIColor.systemGray6))
                         .cornerRadius(8)
-                        .shadow(radius: 5) // 增加陰影效果
+                        .shadow(radius: 5)
                     
-                    // 登入按鈕
+                    // 登入按鈕 (Email & Password)
                     Button(action: {
-                        navigateToHome = true
+                        loginWithEmailPassword()  // Email & Password 登录逻辑
                     }) {
                         Text("登入")
                             .foregroundColor(.white)
@@ -62,14 +63,12 @@ struct LoginDetailView: View {
                             .cornerRadius(8)
                             .shadow(radius: 5)
                     }
-                    .sheet(isPresented: $navigateToHome) {
-                        MainTabView()
-                    }
-                    
-                    
+                    NavigationLink(destination: MainTabView(), isActive: $navigateToHome) {
+EmptyView()  // 隐藏的链接，只在登录成功后触发
+}
+
                     // 忘記密碼按鈕
                     Button(action: {
-                        print("忘記密碼按钮被點擊")
                         navigateToForgotPassword = true
                     }) {
                         Text("忘記密碼?")
@@ -92,40 +91,33 @@ struct LoginDetailView: View {
                         .foregroundColor(.gray)
                         .padding(.top, 10)
                     
-                    // Sign In Button
-                    VStack(alignment: .leading) {
-                        SignInWithAppleButton(.signIn) { request in
-                            let nonce = randomNonceString()
-                            self.nonce = nonce
-                            //Yout Preference
-                            request.requestedScopes = [.email, .fullName]
-                            request.nonce = sha256(nonce)
-                        } onCompletion: { result in
-                            switch result {
-                            case .success(let authorization) :
-                                loginWithFirebase(authorization)                            case .failure(let error):
-                                showError(error.localizedDescription)
-                            }
+                    // Apple Sign In
+                    SignInWithAppleButton(.signIn) { request in
+                        let nonce = randomNonceString()
+                        self.nonce = nonce
+                        request.requestedScopes = [.email, .fullName]
+                        request.nonce = sha256(nonce)
+                    } onCompletion: { result in
+                        switch result {
+                        case .success(let authorization):
+                            loginWithFirebase(authorization)  // Apple 登录
+                        case .failure(let error):
+                            showError(error.localizedDescription)
                         }
-                        .overlay {
-                            ZStack {
-                                Capsule()
-                                
-                                HStack {
-                                    Image(systemName: "applelogo")
-                                    
-                                    Text("Sign in with Apple")
-                                }
-                                .foregroundStyle(scheme == .dark ? .black : .white)
-                            }
-                            .allowsHitTesting(false)
-                        }
-                        .frame(height: 45)
-                        .clipShape(.capsule)
                     }
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(5)
-                    
+                    .frame(height: 45)
+                    .clipShape(.capsule)
+                    .overlay {
+                        ZStack {
+                            Capsule()
+                            HStack {
+                                Image(systemName: "applelogo")
+                                Text("Sign in with Apple")
+                            }
+                            .foregroundStyle(scheme == .dark ? .black : .white)
+                        }
+                        .allowsHitTesting(false)
+                    }
                 }
                 .padding()
                 Image("monster")
@@ -146,15 +138,19 @@ struct LoginDetailView: View {
         }
     }
     
-    @ViewBuilder
-    func LoadingScreen() -> some View {
-        ZStack {
-            Rectangle()
-                .fill(.ultraThinMaterial)
-            
-            ProgressView()
-                .frame(width: 45, height: 45)
-                .background(.background, in: .rect(cornerRadius: 5))
+    // Email & Password 登录逻辑
+    func loginWithEmailPassword() {
+        isLoggedIn = true
+        Auth.auth().signIn(withEmail: loginViewModel.email, password: loginViewModel.password) { authResult, error in
+            if let error = error {
+                showError(error.localizedDescription)
+                isLoggedIn = false
+                return
+            }
+            // 登录成功，更新 logStatus 并导航到 MainTabView
+            logStatus = true
+            isLoggedIn = false
+            navigateToHome = true
         }
     }
     
@@ -164,74 +160,69 @@ struct LoginDetailView: View {
         isLoggedIn = false
     }
     
+    // Apple ID 登录逻辑
     func loginWithFirebase(_ authorization: ASAuthorization) {
         if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
             
-            isLoggedIn = true
-            
             guard let nonce else {
                 fatalError("Invalid state: A login callback was received, but no login request was sent.")
-                showError("Cannot process your request.")
-                return
             }
             guard let appleIDToken = appleIDCredential.identityToken else {
-                print("Unable to fetch identity token")
-                showError("Cannot process your request.")
+                showError("Unable to fetch identity token")
                 return
             }
             guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
-                print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
-                showError("Cannot process your request.")
+                showError("Unable to serialize token string from data")
                 return
             }
-            // Initialize a Firebase credential, including the user's full name.
-            let credential = OAuthProvider.appleCredential(withIDToken: idTokenString,
-                                                           rawNonce: nonce,
-                                                           fullName: appleIDCredential.fullName)
-            // Sign in with Firebase.
-            Auth.auth().signIn(with: credential) { (authResult, error) in
-                if let error {
-                    // Error. If error.code == .MissingOrInvalidNonce, make sure
-                    // you're sending the SHA256-hashed nonce as a hex string with
-                    // your request to Apple.
+            
+            let credential = OAuthProvider.appleCredential(withIDToken: idTokenString, rawNonce: nonce, fullName: appleIDCredential.fullName)
+            Auth.auth().signIn(with: credential) { authResult, error in
+                if let error = error {
                     showError(error.localizedDescription)
+                    return
                 }
-                // User is signed in to Firebase with Apple.
-                // Pushing User to MainTabView
+                // Apple 登录成功
                 logStatus = true
                 isLoggedIn = false
+                navigateToHome = true
             }
         }
     }
+    
     private func randomNonceString(length: Int = 32) -> String {
         precondition(length > 0)
         var randomBytes = [UInt8](repeating: 0, count: length)
         let errorCode = SecRandomCopyBytes(kSecRandomDefault, randomBytes.count, &randomBytes)
         if errorCode != errSecSuccess {
-            fatalError(
-                "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
-            )
+            fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
         }
         
         let charset: [Character] =
-        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
-        
-        let nonce = randomBytes.map { byte in
-            // Pick a random character from the set, wrapping around if needed.
-            charset[Int(byte) % charset.count]
-        }
-        
-        return String(nonce)
-    }
+               Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+
+               let nonce = randomBytes.map { byte in
+                   // Pick a random character from the set, wrapping around if needed.
+                   charset[Int(byte) % charset.count]
+               }
+       
+               return String(nonce)
+           }
     
     private func sha256(_ input: String) -> String {
         let inputData = Data(input.utf8)
         let hashedData = SHA256.hash(data: inputData)
-        let hashString = hashedData.compactMap {
-            String(format: "%02x", $0)
-        }.joined()
-        
-        return hashString
+        return hashedData.map { String(format: "%02x", $0) }.joined()
+    }
+    
+    @ViewBuilder
+    func LoadingScreen() -> some View {
+        ZStack {
+            Rectangle().fill(.ultraThinMaterial)
+            ProgressView()
+                .frame(width: 45, height: 45)
+                .background(.background, in: .rect(cornerRadius: 5))
+        }
     }
 }
 
